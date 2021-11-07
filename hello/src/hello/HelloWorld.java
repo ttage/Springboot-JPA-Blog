d65c977d0e7f8159d23804c5fad9deca6a9f7e75
unknown <nanamja77@naver.com>
2021-11-07 11:14:26 +0900

    hello world

---
+++ hello/src/hello/HelloWorld.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 스프링부트 연습
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
+
